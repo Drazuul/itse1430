d45560ca9c465ec1fd7956313638b6d409240398
Drazuul <ivey.j97@gmail.com>
2019-08-26 18:56:06 -0500

    class 8/26/2019

--- classwork/Section1/HelloWorld/Program.cs
+++ classwork/Section1/HelloWorld/Program.cs
@@ -4,11 +4,23 @@ namespace HelloWorld
 {
     class Program
     {
-        static void Main ( string[] args )
+        static void Main ( /*string[] args*/ )
         {
+            string name = "";
+
+            string name2 = Console.ReadLine();
+            Console.WriteLine (name2);
             Console.WriteLine ("Hello World!");
 
-            // This comment is a change to the file
+            double payRate = 15.25;
+            int hours = 8;
+
+            double totalPay = payRate * hours;
+
+            string fullName = "Fred" + " " + "Jones";
+
+
+
         }
     }
 }
