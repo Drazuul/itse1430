d88b0998316b4fd309a4f5fc64a598f27c0a7e1e
Drazuul <ivey.j97@gmail.com>
2019-09-09 19:01:06 -0500

    class 9/9

--- classwork/Section1/HelloWorld/Program.cs
+++ classwork/Section1/HelloWorld/Program.cs
@@ -1,61 +1,112 @@
-﻿using System;
+﻿/*
+    Jacob Ivey
+    section 1
+ */
+using System;
 
 namespace HelloWorld
 {
     class Program
     {
-        static void Main ( string[] args )
+        static void Main ( /*string[] args*/ )
         {
-            //Movie Data
-            string title;
-            int runLength;
-            int releaseYear;
-            string description;
-            bool haveSeen;
+            //Movie data
+            //string title;
+            //int runLength;
+            //int releaseYear;
+            //string description;
+            //bool haveSeen;
+            var quit = false;
 
-            while (true)
+            while (!quit)
             {
                 char input = DisplayMenu ();
-                if (input == 'A')
-                    AddMovie ();
-                else if (input == 'Q')
-                   break;
+
+                switch (input)
+                {
+                    //fallthrough only if empty case
+                    case 'a':
+                    case 'A': AddMovie (); break;
+
+                    //must have break or return at end of case
+                    case 'D': DisplayMovie (); break;
+                    case 'R': RemoveMovie (); break;
+                    case 'Q':
+                    {
+                        quit = true;
+                        break;
+                    }
+                    default: Console.WriteLine ("Not supported"); break;
+                }
+                //if (input == 'A')
+                //    AddMovie ();
+                //else if (input == 'D')
+                //    DisplayMovie ();
+                //else if (input == 'Q')
+                //    break;
+                //else if (input == 'R')
+                //    RemoveMovie ();
             };
         }
 
-        static void AddMovie()
+        private static void RemoveMovie ()
         {
-            //Get Title
-            Console.Write ("Title: ");
-            string title = Console.ReadLine ();
-            //Get Description
-            Console.Write ("Description: ");
-            string description = Console.ReadLine ();
-            //Get Release year
-            int releaseYear = ReadInt32 ("Release Year: ");
+            //confirm removal
+            if (!ReadBoolean ($"Are you sure you want to remove {title}?"))
+                return;
 
-            //Get run length
-            int runLength = ReadInt32 ("Run Length (in minutes): ");
-            //Get have seen
-            Console.Write ("Have Seen? ");
-            bool haveSeen = ReadBoolean ("Have Seen? ");
+            //remove movie
+            title = null;
         }
 
-        static int ReadInt32 ( string message )
+        static void AddMovie ()
         {
-            while (true)
+            //Get title
+            Console.Write("Title: ");
+            title = Console.ReadLine();
+
+            //Get description
+            Console.Write ("Description: ");
+            description = Console.ReadLine ();
+
+            //Get release year
+            releaseYear = ReadInt32("Release Year: ");
+            
+            //Get run length
+            runLength = ReadInt32("Run Length (in minutes): ");
+
+            //Get have seen
+            hasSeen = ReadBoolean("Have Seen? ");
+        }
+
+        static void DisplayMovie ()
+        {
+            //Display message if no movies
+            if (String.IsNullOrEmpty(title))
             {
-                Console.Write (message);
-                string input = Console.ReadLine ();
-
-                //int result = Int32.Parse (input);
-               // int result;
-                //if (Int32.TryParse (input, out result))
-                if (Int32.TryParse(input, out int result))
-                    return result;
-
-                Console.WriteLine ("Not a number.");
+                Console.WriteLine ("no movies");
+                return;
             }
+            
+            //Title, description, release year, run length, hasSeen
+            Console.WriteLine(title);
+            Console.WriteLine(description);
+
+            //Formatting strings
+            //1) String concat
+            Console.WriteLine("Released " + releaseYear);
+
+            //2) Printf
+            //Console.WriteLine("Run time: {0}", runLength);
+
+            //3) String formatting
+            var formattedString = String.Format("Run time: {0}", runLength);
+            Console.WriteLine(formattedString);
+
+            //4) String interpolation
+            Console.WriteLine($"Seen it? {hasSeen}");
+
+            Console.WriteLine("".PadLeft(50, '-'));
         }
 
         static bool ReadBoolean ( string message )
@@ -63,6 +114,7 @@ namespace HelloWorld
             while (true)
             {
                 Console.Write (message);
+
                 string input = Console.ReadLine ();
 
                 //int result = Int32.Parse (input);
@@ -70,44 +122,211 @@ namespace HelloWorld
                 if (Boolean.TryParse (input, out result))
                     return result;
 
-                Console.WriteLine ("Not a boolean.");
-            }
+                Console.WriteLine ("Not a boolean");
+            };
         }
+
+        static int ReadInt32 ( string message )
+        {
+            while (true)
+            {
+                Console.Write (message);
+
+                var input = Console.ReadLine ();
+
+                //int result = Int32.Parse (input);
+                //int result;
+                //if (Int32.TryParse (input, out result))
+                if (Int32.TryParse(input, out var result))
+                    return result;
+
+                Console.WriteLine ("Not a number");
+            };
+        }
+
         static char DisplayMenu ()
         {
             do
             {
+                Console.WriteLine("A)dd Movie");
+                Console.WriteLine("D)isplay Movie");
+                Console.WriteLine("R)emovee Movie");
+                Console.WriteLine("Q)uit");
 
-            Console.WriteLine("A)dd Movie");
-            Console.WriteLine("Q)uit");
 
-            string input = Console.ReadLine ();
-            if (input == "A" || input == "a")
-            {
+                string input = Console.ReadLine();
+
+                //Lower case
+                input = input.ToLower();
+                //if (input == "A" || input == "a")
+                //if (input == "a")
+                if (String.Compare (input, "a", true) == 0)
+                {
                     return 'A';
-            } else if (input == "Q" || input == "q")
-
-            {
+                } else if (input == "q")
+                {
                     return 'Q';
-            } else
-                Console.WriteLine("Invalid input.");
-            }while(true);
+                } else if (input == "d")
+                    return 'D';
+                else if (input == "r")
+                    return 'R';
+                else
+                    Console.WriteLine ("Invalid input");
+
+            } while(true);
         }
 
         private static void DemoLanguage ()
         {
+            //TODO: Move this
             string name = "";
 
+            //string if = "";
+
+            //Definitely assigned
+            //name = "Bob";
             string name2 = Console.ReadLine ();
+            //name2 = Console.ReadLine ();
+
+            name2 = name = "Sue";
             Console.WriteLine (name2);
             Console.WriteLine ("Hello World!");
 
-            double payRate = 15.25;
+            //Another block
+            //Yet another block
+
             int hours = 8;
+            double payRate = 15.25;
 
             double totalPay = payRate * hours;
 
             string fullName = "Fred" + " " + "Jones";
+
+            DemoArithmetic ();
+            DemoRelational ();
+            DemoLogical ();
+            DemoConditional ();
         }
+
+        static void DemoArithmetic ()
+        {
+            int hours = 8;
+            double payRate = 15.25;
+
+            double totalPay = hours * payRate;
+
+            double taxes = totalPay * 0.33;
+
+            //Combination operators work
+            hours += 8;
+
+            totalPay = hours * payRate;
+            double newTaxes = totalPay * 0.66;
+        }
+
+        static void DemoRelational ()
+        {
+            int grade = 75;
+            int passingGrade = 60;
+
+            bool isGreater = grade > passingGrade;
+            bool isGreaterThanOrEqual = grade >= passingGrade;
+            bool isLessThan = grade < passingGrade;
+            bool isLessThanOrEqual = grade <= passingGrade;
+            bool isEqual = grade == passingGrade;
+            bool isNotEqual = grade != passingGrade;
+        }
+
+        static void DemoLogical ()
+        {
+            bool value1 = true;
+            bool value2 = false;
+
+            //Both left and right must be true
+            bool logicalAnd = value1 && value2;
+
+            //Either left or right must be true
+            bool logicalOr = value1 || value2;
+
+            //Negates the value
+            bool logicalNot = !value1;
+        }
+
+        static void DemoConditional ()
+        {
+            int grade = 75;
+
+            // C ? T : F
+            // Evaluate C, if true then T is the value of the expression
+            //otherwise F is
+            bool isPassing = grade > 60 ? true : false;
+
+            //Equivalent to what an if statement could do if it were an expression
+            if (grade > 60)
+                isPassing = true;
+            else
+                isPassing = false;
+        }
+
+        static void DemoArray ()
+        {
+            double[] payRates = new double[100];
+
+            //50th element to 7.25
+            payRates[49] = 7.25;
+
+            //Read 89th element into temp variable
+            double payRate = payRates[88];
+
+            //Print out the 80th element
+            Console.WriteLine(payRates[79]);
+
+            //An empty array
+            //bool[] isPassing = new bool[0];
+            var isPassing = new bool[0];
+
+            //Enumerating an array
+            for (int index = 0; index < payRates.Length; ++index)
+                Console.WriteLine(payRates[index]);
+
+            //Type inferencing
+            //string name = "Bob William Smith Jr III";
+            var name = "Bob William Smith Jr III";
+            //name = 20;            
+
+            string[] nameParts = name.Split(' ');
+        }
+
+        static void DemoString()
+        {
+            string str = null;
+
+            //checking for null
+            if (str != null)
+                str.ToLower ();
+
+            //checking for null or empty string
+            if (str != null && str != "")
+                str = str.ToLower ();
+
+            //Length - no
+            if (str != null && str.Length == 0)
+               str = str.ToLower ();
+
+            //Empty - no
+            if (str != null && str != String.Empty)
+                str = str.ToLower ();
+
+            //correct approach
+            if (!String.IsNullOrEmpty (str))
+                str = str.ToLower ();
+        }
+
+        //Don't do this outside lab 1
+        static string title;
+        static string description;
+        static int runLength;
+        static int releaseYear;
+        static bool hasSeen;
     }
 }
