342dce1c340d0af5c7d7a357d4c052579aed5dec
Drazuul <ivey.j97@gmail.com>
2019-09-11 19:11:17 -0500

    class 9/11/2019

---
+++ classwork/MovieLibrary/Itse130.MovieLib/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itse130.MovieLib
+{
+    /// <summary>
+    /// represents movie data.
+    /// </summary>
+    public class Movie
+    {
+    }
+}
---
+++ classwork/MovieLibrary/Itse430.MovieLib.Host/MainForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+using Itse130.MovieLib;
+
+namespace Itse430.MovieLib.Host
+{
+    public partial class MainForm : Form
+    {
+        public MainForm ()
+        {
+            InitializeComponent ();
+
+            Movie movie = new Movie ();
+        }
+    }
+}
