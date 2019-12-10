9a55eadde062893be7aaa567d6260649688e05e2
Jacob Ivey <ivey.j97@gmail.com>
2019-12-10 03:23:11 -0600

    Finished Lab 5

---
+++ labs/startercode/Nile.Web/Controllers/ProductController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Nile.Stores.Sql;
+using Nile.Web.Models;
+
+
+namespace Nile.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ProductController ()
+        {
+            var connString = ConfigurationManager.ConnectionStrings["ProductDatabase"];
+            _database = new SqlProductDatabase (connString.ConnectionString);
+        }
+
+        [HttpGet]
+        public ActionResult Index ()
+        {
+            var items = _database.GetAll ()
+                                 .OrderBy (x => x.Name);
+
+            var model = items.Select (i => i.ToModel ());
+
+            return View (model);
+        }
+
+        [HttpGet]
+        public ActionResult Create ()
+        {
+            return View ();
+        }
+
+        [HttpGet]
+        public ActionResult Delete ( int id )
+        {
+            var product = _database.Get (id);
+            if (product == null)
+                return HttpNotFound ();
+
+            var model = product.ToModel ();
+            return View (model);
+        }
+
+        [HttpPost]
+        public ActionResult Delete ( ProductModel model )
+        {
+            try
+            {
+                _database.Remove (model.Id);
+
+                return RedirectToAction ("Index");
+            } catch (Exception e)
+            {
+                ModelState.AddModelError ("", e.Message);
+            };
+
+            return View (model);
+        }
+
+        [HttpGet]
+        public ActionResult Edit ( int id )
+        {
+            var product = _database.Get (id);
+            if (product == null)
+                return HttpNotFound ();
+
+            var model = product.ToModel ();
+            return View (model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit ( ProductModel model )
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var product = model.ToDomain ();
+                    _database.Update (product); //overload broken
+
+                    return RedirectToAction ("Edit", new { id = product.Id });
+                };
+            } catch (Exception e)
+            {
+                ModelState.AddModelError ("", e.Message);
+            };
+
+            return View (model);
+        }
+
+        [HttpPost]
+        public ActionResult Create ( ProductModel model )
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var product = model.ToDomain ();
+                    _database.Add (product);
+
+                    return RedirectToAction ("Index");
+                };
+            } catch (Exception e)
+            {
+                ModelState.AddModelError ("", e.Message);
+            };
+
+            return View (model);
+        }
+
+        [HttpGet]
+        public ActionResult Details ( int id )
+        {
+            var product = _database.Get (id);
+            if (product == null)
+                return HttpNotFound ();
+
+            var model = product.ToModel ();
+            return View (model);
+        }
+
+        private readonly IProductDatabase _database;
+    }
+}
---
+++ labs/startercode/Nile.Web/Models/ProductModelExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace Nile.Web.Models
+{
+    public static class ProductModelExtensions
+    {
+        public static Product ToDomain (this ProductModel source)
+        {
+            if (source == null)
+                return null;
+
+            return new Product () {
+                Id =source.Id,
+                Name = source.Name,
+                Description = source.Description,
+                Price = source.Price,
+                IsDiscontinued = source.IsDiscontinued
+            };
+        }
+
+        public static ProductModel ToModel ( this Product source )
+        {
+            if (source == null)
+                return null;
+
+            return new ProductModel() {
+                Id =source.Id,
+                Name = source.Name,
+                Description = source.Description,
+                Price = source.Price,
+                IsDiscontinued = source.IsDiscontinued
+            };
+        }
+    }
+}
